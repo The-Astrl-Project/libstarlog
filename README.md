f6c4d7f1b43e10c043e1cf903de5b5fafdf74995
Maxeeme <146043018+MaxineToTheStars@users.noreply.github.com>
2024-09-22 13:29:10 -0400

    Project status upload
    Mostly just project structuring and organization

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# libstarlog
- A feature rich logging utility
+<!-- Move text down -->
+<br>
+
+<!-- Header -->
+<h1 align="center">libStarlog</h1>
+
+<!-- Subheading -->
+<h3 align="center">A feature rich logging utility</h3>
---
+++ include/logging.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2024 Astrl.
+ *
+ * This file is part of libStarlog. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this project and at
+ * https://github.com/The-Astrl-Project/libstarlog/blob/HEAD/LICENSE.
+ *
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+
+// Header Declarations
+// ----------------------------------------------------------------
+
+// ---
+
+// ---
+
+// ----------------------------------------------------------------
+
+// Macro Definitions
+
+// File Docstring
+// --------------------------------
+// libStarlog || src/logging.c <-> include/logging.h
+//
+// Provides the logging utility from libStarlog.
+//
+// @author @MaxineToTheStars <https://github.com/MaxineToTheStars>
+// ----------------------------------------------------------------
+
+// Struct Definitions
+
+// Enum Definitions
+
+// Type Definitions
+
+// Variable Definitions
+
+// Main
+
+// Methods
